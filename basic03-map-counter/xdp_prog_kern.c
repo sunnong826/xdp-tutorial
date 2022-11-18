7be6319f2f6804833ce3d6c0e57f9b43a0a099fc
sunnong826 <83113781+sunnong826@users.noreply.github.com>
2022-11-18 17:03:40 +0900

    Update xdp_prog_kern.c

--- basic03-map-counter/xdp_prog_kern.c
+++ basic03-map-counter/xdp_prog_kern.c
@@ -25,10 +25,11 @@ struct bpf_map_def SEC("maps") xdp_stats_map = {
 SEC("xdp_stats1")
 int  xdp_stats1_func(struct xdp_md *ctx)
 {
-	// void *data_end = (void *)(long)ctx->data_end;
-	// void *data     = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
 	struct datarec *rec;
 	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
+	__u64 bytes = data_end - data;
 
 	/* Lookup in kernel BPF-side return pointer to actual data record */
 	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
@@ -43,6 +44,7 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	 * use an atomic operation.
 	 */
 	lock_xadd(&rec->rx_packets, 1);
+	lock_xadd(&rec->rx_bytes, bytes);
         /* Assignment#1: Add byte counters
          * - Hint look at struct xdp_md *ctx (copied below)
          *
